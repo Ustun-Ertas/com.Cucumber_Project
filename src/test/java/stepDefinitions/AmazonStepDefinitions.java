a9428ed46224674c8096272700c125d83597a7d1
Ustun Ertas <ustunertas0@gmail.com>
2022-06-04 22:35:08 +0200

    new project

--- src/test/java/stepDefinitions/AmazonStepDefinitions.java
+++ src/test/java/stepDefinitions/AmazonStepDefinitions.java
@@ -1,5 +1,6 @@
 package stepDefinitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
@@ -9,32 +10,50 @@ import utilities.ConfigReader;
 import utilities.Driver;
 
 public class AmazonStepDefinitions {
-
-    AmazonPage amazonPage = new AmazonPage();
+    AmazonPage amazonPage=new AmazonPage();
 
     @Given("kullanici amazon anasayfasinda")
     public void kullanici_amazon_anasayfasinda() {
-
         Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
-
     }
     @Then("kullanici Nutella icin arama yapar")
     public void kullanici_nutella_icin_arama_yapar() {
-
         amazonPage.aramaKutusu.sendKeys("Nutella" + Keys.ENTER);
-
-
     }
     @Then("sonuclarin Nutella icerdigini test eder")
     public void sonuclarin_nutella_icerdigini_test_eder() {
         String arananKelime="Nutella";
-        String actualText = amazonPage.aramaSonucElementi.getText();
-        Assert.assertTrue(actualText.contains(arananKelime));
+        String actualAramaSonucStr= amazonPage.aramaSonucElementi.getText();
 
+        Assert.assertTrue(actualAramaSonucStr.contains(arananKelime));
     }
     @Then("sayfayi kapatir")
     public void sayfayi_kapatir() {
         Driver.closeDriver();
+    }
+    @Then("kullanici Java icin arama yapar")
+    public void kullanici_java_icin_arama_yapar() {
+        amazonPage.aramaKutusu.sendKeys("Java" + Keys.ENTER);
+    }
 
+    @Then("sonuclarin Java icerdigini test eder")
+    public void sonuclarin_java_icerdigini_test_eder() {
+        String arananKelime="Java";
+        String actualAramaSonucStr= amazonPage.aramaSonucElementi.getText();
+
+        Assert.assertTrue(actualAramaSonucStr.contains(arananKelime));
+    }
+
+    @And("kullanici iphone icin arama yapar")
+    public void kullaniciIphoneIcinAramaYapar() {
+        amazonPage.aramaKutusu.sendKeys("iphone" + Keys.ENTER);
+    }
+
+    @Then("sonuclarin iphone icerdigini test eder")
+    public void sonuclarinIphoneIcerdiginiTestEder() {
+        String arananKelime="iphone";
+        String actualAramaSonucStr= amazonPage.aramaSonucElementi.getText();
+
+        Assert.assertTrue(actualAramaSonucStr.contains(arananKelime));
     }
 }
--- target/test-classes/features/amazonSearch.feature
+++ target/test-classes/features/amazonSearch.feature
@@ -8,3 +8,14 @@ Feature: Amazon Search
     And sonuclarin Nutella icerdigini test eder
     And sayfayi kapatir
 
+  Scenario: TC02  kullanici amazonda Java aratir
+    Given kullanici amazon anasayfasinda
+    Then kullanici Java icin arama yapar
+    And sonuclarin Java icerdigini test eder
+    And sayfayi kapatir
+
+  Scenario: TC03 kullanici iphone aratir
+    When kullanici amazon anasayfasinda
+    And kullanici iphone icin arama yapar
+    Then sonuclarin iphone icerdigini test eder
+    And sayfayi kapatir
